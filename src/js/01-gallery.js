2bea73cf7954615ebc304b4b61923d0790506ba3
andreshkivVC <109181911+andreshkivVC@users.noreply.github.com>
2022-12-05 21:31:06 +0200

    1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+// // Change code below this line
+
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+
+const cardsContainer = document.querySelector('.gallery');
+const cardsMarkup = creatGalleryCard(galleryItems);
+
+cardsContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function creatGalleryCard(images) {
+  return images
+    .map(
+      ({ preview, original, description }) =>
+        ` <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    )
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});

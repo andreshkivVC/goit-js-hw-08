29766e3c516729d9f46a41038f9d13047bf05912
andreshkivVC <109181911+andreshkivVC@users.noreply.github.com>
2022-12-06 20:52:13 +0200

    2 tack

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,43 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem('videoplayer', JSON.stringify(data.seconds));
+  }),
+  1000
+);
+
+const data = localStorage.getItem('videoplayer');
+const parsetTime = JSON.parse(data);
+console.log(parsetTime);
+
+player
+  .setCurrentTime(parsetTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video's duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });

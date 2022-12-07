a851b1ab6990870aeded58621c93244729a97ff2
andreshkivVC <109181911+andreshkivVC@users.noreply.github.com>
2022-12-07 07:08:50 +0200

    3 завдання
     3 завдання не дороблене, зняйте відео де в мене помилки.

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from "lodash.throttle";
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  inputEmeil: document.querySelector('input[type = "email"]'),
+  inputMessage: document.querySelector('textarea[name="message"]'),
+};
+
+savedMessage();
+
+console.log(refs.form);
+console.log(refs.inputEmeil);
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onTextareaInput, 500));
+
+function onFormSubmit(evt) {
+    evt.preventDefaut();
+    JSON.parse(localStorage.getItem(STORAGE_KEY));
+    console.log(formData);
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+function savedMessage() {
+    const saveMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (saveMessage) {
+        refs.inputMessage.value = saveMessage.formData.message;
+        refs.inputEmeil.value = saveMessage.formData.email;
+        console.log(saveMessage.formData.message);
+    }
+}
+
+function onTextareaInput(evt) {
+  let fg = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  formData = { ...fg, [evt.target.name]: evt.target.value };
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+
+}
